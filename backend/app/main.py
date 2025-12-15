fcca682a22f8f8b4411c0857a8edcce4a7912fbb
Sunny Gogoi <sgogoi2004@gmail.com>
2025-12-15 11:49:05 +0530

    Modifying CORS for frontend access

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     #     "http://localhost:3000",
     #     "https://product-discovery-assistant-one.vercel.app/",
     # ],
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
